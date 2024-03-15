1b61922f551a8235ecdc8b4b858d49728cebb495
kongimi <38864585+kongimi@users.noreply.github.com>
2024-03-15 19:48:55 +0700

    Add files via upload

--- ft_printf.c
+++ ft_printf.c
@@ -3,61 +3,172 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: npiyapan <npiyapan@student.42.fr>          +#+  +:+       +#+        */
+/*   By: npiyapan <niran.analas@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/07 14:55:09 by npiyapan          #+#    #+#             */
-/*   Updated: 2024/03/07 15:35:59 by npiyapan         ###   ########.fr       */
+/*   Created: 2024/03/15 10:14:21 by npiyapan          #+#    #+#             */
+/*   Updated: 2024/03/15 19:43:36 by npiyapan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <limits.h>
 
-int	ft_write(int c)
+int find_len(long c, int base)
 {
-	write (1, &c, 1);
-	return (1);
-}
+	int len;
+	int my_base;
+	long buf;
 
-int	ft_check(va_list *args, const char *str)
-{
-	int	len;
-
-	len = 0;
-	if (*str == 'c')
-		len += ft_write(va_arg(*args, int));
+	len  = 1;
+	my_base = base;
+	buf = c;
+	if (buf < 0)
+	{
+		len++;
+		buf *= (-1);
+	}
+	while (buf >= my_base)
+	{
+		len++;
+		buf /= my_base;
+	}
 	return (len);
 }
 
-int	ft_printf(const char *str, ...)
+char *ft_itoa(long c, int base)
 {
-	va_list	args;
-	int		len;
-	int		i;
+	char *res;
+	int len;
+	long buf;
+	int i;
+	char *my_num = "0123456789abcdef";
 
-	len = 0;
+	len = find_len(c, base);
+	res = malloc(sizeof(char) * len + 1);
+	buf = c;
 	i = 0;
-	va_start(args, str);
+	if (buf < 0)
+	{
+		buf *= (-1);
+	}
+	while (i <= len)
+	{
+		res[len - i - 1] = my_num[buf % base];
+		buf /= base;
+		i++;
+	}
+	if (c < 0)
+		res[0] = '-';
+	res[len] = 0;
+	return (res);
+}
+
+int ft_writenum(long c, int base)
+{
+	int len;
+	int i;
+	char *res;
+
+	i = 0;
+	res = ft_itoa(c, base);
+	len = 0;
+	while (res[i])
+	{
+		len += write(1, res + i, 1);
+		i++;
+	}
+	free (res);
+	return (len);
+}
+
+int ft_writestr(char *str)
+{
+	int len;
+	int i;
+
+	i = 0;
+	len  = 0;
 	while (str[i])
 	{
-		if (str[len] == '%')
+		len += write(1, str + i, 1);
+		i++;
+	}
+	return (len);
+}
+
+int check(va_list args, char str)
+{
+	int len;
+
+	len  = 0;
+	if (str == 's')
+		len += ft_writestr(va_arg(args, char *));
+	else if (str == 'd')
+		len += ft_writenum(va_arg(args, int), 10);
+		else if (str == 'x')
+		len += ft_writenum(va_arg(args, unsigned int), 16);
+	return (len);
+}
+
+int ft_printf(const char *str, ...)
+{
+	va_list args;
+	int len;
+	int i;
+
+	va_start(args, str);
+	i = 0;
+	len = 0;
+	while (str[i])
+	{
+		if (str[i] == '%')
 		{
+			len += check(args, str[i + 1]);
 			i++;
-			len += ft_check(&args, str + i);
 		}
 		else
-			len += write (1, &str[len], 1);
+			len += write(1, str + i, 1);
 		i++;
 	}
 	va_end(args);
 	return (len);
 }
 
-int	main(void)
+int main(void)
 {
-	char	m;
+	char *str = "abc";
+	int len;
+	int a;
+	int b;
 
-	m = '2';
-	ft_printf("test = %c", m);
+	len = ft_printf("test\n");
+	printf("len = %d\n", len);
+	len = ft_printf("%s\n", str);
+	printf("len = %d\n", len);
+	a = 2345;
+	len = ft_printf("%d\n", a);
+	printf("len = %d\n", len);
+	b = -12345;
+	len = ft_printf("%d\n", b);
+	printf("len = %d\n", len);
+	a = 255;
+	len = ft_printf("%x\n", a);
+	printf("len = %d\n", len);
+	a = -16;
+	len = ft_printf("%x\n", a);
+	printf("len = %d\n", len);
+	a = INT_MAX;
+	ft_printf("INT_MAX = %x\n", a);
+	printf("INT_MAX original = %x\n", a);
+	ft_printf("INT_MAX = %d\n", a);
+	printf("INT_MAX original = %d\n", a);
+	a = INT_MIN;
+	ft_printf("INT_MIN = %x\n", a);
+	printf("INT_MIN original = %x\n", a);
+	ft_printf("INT_MIN = %d\n", a);
+	printf("INT_MIN original = %d\n", a);
+
 }
